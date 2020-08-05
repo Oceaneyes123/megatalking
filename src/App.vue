02d81b57afb3bdb553233defac70eb5db2eba118
dave <jezreldavesondia@gmail.com>
2020-08-05 19:42:55 +0800

    8-5

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <v-app>
+      <v-card tile height="75" width="100%">
+        <v-container class="d-flex flex-row">
+          <v-card flat tile color="#00000000">
+            <v-img contain src="../src/assets/logo.png" width="50"></v-img>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-card flat class="d-flex align-center mr-5" color="#00000000">
+            <div class="mr-4 subheading">커리큘럼</div>
+            <div class="mr-4 subheading">레벨테스트</div>
+            <div class="mr-4 subheading">수강신청</div>
+            <div class="mr-8 subheading">마이페이지</div>
+            <div class="mr-4 subheading orange--text text--lighten-1">KR</div>
+            <div class="mr-4 subheading">EN</div>
+            <div class="mr-4 subheading">
+              <a>로그인</a>
+            </div>
+          </v-card>
+        </v-container>
+      </v-card>
+
+      <router-view />
+    </v-app>
   </div>
 </template>
 
@@ -13,20 +31,36 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.h1 {
+  font-weight: 300;
+  font-size: 6rem;
+  letter-spacing: -0.09375rem;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.h2 {
+  font-weight: 300;
+  font-size: 3.75rem;
+  letter-spacing: -0.03125rem;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.h3 {
+  font-weight: 400;
+  font-size: 3rem;
+  letter-spacing: normal;
+}
+.h4 {
+  font-weight: 400;
+  font-size: 2.125rem;
+  letter-spacing: -0.015625remm;
+}
+.h5 {
+  font-weight: 400;
+  font-size: 1.5rem;
+  letter-spacing: normal;
+}
+.h6 {
+  font-weight: 500;
+  font-size: 1.25rem;
+  letter-spacing: 0.009375rem;
 }
 </style>
---
+++ src/views/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <v-container fluid class="py-0 px-0">test</v-container>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
